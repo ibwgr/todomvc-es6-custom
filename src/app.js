a1301b24830639293f39db6c5791cac9a9701794
Ueli Kunz <kunz@ideadapt.net>
2017-02-10 22:30:24 +0100

    bring controller into the game. it will late connect the view with the model (store)

--- src/app.js
+++ src/app.js
@@ -2,8 +2,10 @@
 
 import Store from './store'
 import View from './view'
+import Controller from './controller'
 
 const view = new View('#the-app')
+const ctrl = new Controller(view)
 
 window.addEventListener('load', () => {
     //view.render()
